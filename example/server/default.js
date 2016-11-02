0a3bd88c1354d9e086bdd587d5044ae35c66798d
Wildhoney <adam.timberlake@gmail.com>
2016-11-02 18:24:26 +0000

    Updated server endpoints and env var for current domain

--- example/server/default.js
+++ example/server/default.js
@@ -3,11 +3,13 @@ import { existsSync } from 'fs';
 import http from 'http';
 import cors from 'cors';
 import express from 'express';
-import { get, create } from 'axios';
-import { camelizeKeys } from 'humps';
+import { generate } from 'shortid';
+import qr from 'qr-image';
 
 const app = express();
 const server = http.createServer(app);
+const domain = 'HEROKU_APP_NAME' in process.env ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com/`
+                                                : process.env.TODO_URL;
 
 app.get('/*',(req, res, next) => {
     res.set('Service-Worker-Allowed', '/');
@@ -17,35 +19,11 @@ app.get('/*',(req, res, next) => {
 app.use(express.static(__dirname + '/example'));
 app.use(cors());
 
-app.get('/position', (request, response) => {
+app.get('/create', (request, response) => {
 
-    const position = get('http://api.open-notify.org/iss-now.json');
-    const astronauts = get('http://api.open-notify.org/astros.json');
-
-    Promise.all([position, astronauts]).then(responses => {
-
-        const [ position, astronauts ] = responses.map(model => camelizeKeys(model.data));
-        const people = astronauts.people.map(person => person.name);
-        const { latitude, longitude } = position.issPosition;
-
-        const instance = create({
-            headers: { 'Accept-Language': 'en-US,en;q=0.8' }
-        });
-
-        const url = `http://nominatim.openstreetmap.org/reverse?format=xml&lat=${latitude}&lon=${longitude}&zoom=18&addressdetails=1&format=json&namedetails=0`;
-
-        instance.get(url).then(model => {
-
-            const country = model.data.address.country;
-            const flagName = country.replace(/\s+/g, '-').toLowerCase();
-            const imagePath = `${__dirname}/example/js/components/iss-position/images/flags/${flagName}.png`;
-            const flag = existsSync(imagePath) ? basename(imagePath) : undefined;
-
-            response.send({ people, country, flag, latitude, longitude });
-
-        }).catch(() => response.send({ people, latitude, longitude }));
-
-    }).catch(response.send(''));
+    const id = generate();
+    const image = qr.imageSync(`${domain}/#/${id}`, { type: 'svg' });
+    response.send({ id, image });
 
 });
 
