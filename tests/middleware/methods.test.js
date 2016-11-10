120b1614079d242cb67f6055ac5e2bd3bd835da5
Wildhoney <adam.timberlake@gmail.com>
2016-11-10 12:25:38 +0000

    Updated 'methods' test to suppress console.error

--- tests/middleware/methods.test.js
+++ tests/middleware/methods.test.js
@@ -1,6 +1,9 @@
 import test from 'ava';
-import { spy } from 'sinon';
+import { spy, restore } from 'sinon';
 import methods from '../../src/middleware/methods';
+
+test.beforeEach(t => spy(window.console.error));
+test.afterEach(t => restore(window.console.error));
 
 test('Should be able to apply methods to a component;', t => {
 
