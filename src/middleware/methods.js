bb18d2a6b6a365c452f93ff7a2443ae3bb63ccd2
Wildhoney <adam.timberlake@gmail.com>
2016-11-19 13:42:14 +0000

    Passing 'arg' if args.length === 1

--- src/middleware/methods.js
+++ src/middleware/methods.js
@@ -28,9 +28,12 @@ const setPrototypeFor = (node, fns) => {
                 return;
             }
 
+            const lastProps = this[lastPropsKey];
+
             // Gather the props that caused the last render of the component, and then invoke
-            // the prototype function.
-            fn({ ...this[lastPropsKey], args });
+            // the prototype function. If only one argument has been passed, then we'll also define
+            // the `arg` variable to make it more gramtically correct.
+            args.length === 1 ? fn({ ...lastProps, args, arg: args[0] }) : fn({ ...lastProps, args });
 
         };
 
