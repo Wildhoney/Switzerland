8d2e717e3def1ca199f3384b87030a768a9da9cd
Wildhoney <adam.timberlake@gmail.com>
2017-01-15 17:36:29 +0000

    Changed 'virtual-dom' for transclude test

--- tests/middleware/transclude.test.js
+++ tests/middleware/transclude.test.js
@@ -1,17 +1,16 @@
 import test from 'ava';
-// import { VNode } from 'virtual-dom';
-// import transclude from '../../src/middleware/transclude';
+import { VNode } from 'virtual-dom-webcomponents-patch';
+import transclude from '../../src/middleware/transclude';
 
 test('Should be able to transclude HTML elements', t => {
 
-    t.pass();
-    // const node = document.createElement('div');
-    // node.innerHTML = '<h1 id="title">Switzerland</h1>';
-    // const props = transclude({ node });
-    //
-    // t.true(props.children instanceof VNode);
-    // t.is(props.children.tagName, 'h1');
-    // t.is(props.children.key, 'title');
-    // t.is(props.children.children[0].text, 'Switzerland');
+    const node = document.createElement('div');
+    node.innerHTML = '<h1 id="title">Switzerland</h1>';
+    const props = transclude({ node });
+
+    t.true(props.children instanceof VNode);
+    t.is(props.children.tagName, 'h1');
+    t.is(props.children.key, 'title');
+    t.is(props.children.children[0].text, 'Switzerland');
 
 });
