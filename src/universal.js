5af01a1d87204928be273bb8f9fc41e191ea0460
Wildhoney <adam.timberlake@gmail.com>
2017-04-01 18:59:48 +0100

    Refactored universal implementation to be standalone

--- src/universal.js
+++ src/universal.js
@@ -1,11 +1,20 @@
+import { defaultProps } from './switzerland';
+import createElement from 'virtual-dom/create-element';
 import { coreKey } from './helpers/keys';
 
 /**
  * @method render
  * @param {Object} component
+ * @param {Object} [props = {}]
  * @return {String}
  */
-export const render = async component => {
-    const result = await component.connected(true);
-    return result.value[coreKey].root;
+export const render = async (component, props = {}) => {
+
+    const node = {};
+    const render = () => {};
+    const attached = true;
+    const result = await component({ node, render, attached, [coreKey]: defaultProps(), ...props, universal: true });
+
+    return createElement(result[coreKey].tree).toString();
+
 };
