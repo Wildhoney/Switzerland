397f03e2b1214d485400dcabe7a68dde10183967
Wildhoney <adam.timberlake@gmail.com>
2017-08-20 14:09:40 +0100

    Updated Uglify Rollup plugin

--- tests/stats/rollup.config.js
+++ tests/stats/rollup.config.js
@@ -4,7 +4,7 @@ import uglify from 'rollup-plugin-uglify';
 import replace from 'rollup-plugin-replace';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import { minify } from 'uglify-js';
+import { minify } from 'uglify-es';
 
 export default {
     format: 'iife',
