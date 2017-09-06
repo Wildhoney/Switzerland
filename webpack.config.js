2eca6edf0f907e4546f12d1ddf94872e658f3c61
Wildhoney <adam.timberlake@gmail.com>
2017-09-06 10:18:32 +0100

    Re-added uglify plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,34 @@ const webpack = require('webpack');
 const Uglify = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    entry: './tests/mocks/welcome-card/index.js',
+    entry: {
+        'core.js': './src/switzerland.js',
+        './tests/mocks/welcome-card/build.js': './tests/mocks/welcome-card/index.js'
+    },
     output: {
-        filename: './tests/mocks/welcome-card/build.js',
+        filename: '[name]',
         libraryTarget: 'var'
     },
     node: {
         fs: 'empty'
     },
     plugins: [
+        new Uglify({
+            uglifyOptions: {
+                ie8: false,
+                ecma: 8,
+                mangle: true,
+                output: {
+                    comments: false,
+                    beautify: false
+                },
+                parse: {
+                    html5_comments: false,
+                    shebang: false
+                },
+                warnings: false
+            }
+        })
     ],
     module: {
         loaders: [
