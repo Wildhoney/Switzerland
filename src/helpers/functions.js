e28104ca4bb1c0350ad9dda8d8b75becd9f4af0f
Wildhoney <adam.timberlake@gmail.com>
2017-09-06 22:25:38 +0100

    Moved "index.html" file

--- src/helpers/functions.js
+++
@@ -1,8 +0,0 @@
-/**
- * @method kebabToCamel
- * @param {String} value
- * @return {String}
- */
-export function kebabToCamel(value) {
-    return value.replace(/(-\w)/g, match => match[1].toUpperCase());
-}
