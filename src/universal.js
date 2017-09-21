460e5163fda1a60ac101e631fbebf86a82afbab2
Wildhoney <adam.timberlake@gmail.com>
2017-09-21 11:56:19 +0100

    Added initial unfinished implementation of universal rendering

--- src/universal.js
+++ src/universal.js
@@ -1,27 +1,65 @@
 import puppeteer from 'puppeteer';
+import express from 'express';
+import cors from 'cors';
+import { basename, dirname } from 'path';
+import { hostname } from 'os';
+import { once } from 'ramda';
+
+/**
+ * @constant defaultOptions
+ * @type {Object}
+ */
+const defaultOptions = {
+    debug: false,
+    hostname: hostname(),
+    protocol: 'http'
+};
+
+/**
+ * @method createServer
+ * @param {String} rootPath
+ * @return {Number}
+ */
+const createServer = once(rootPath => {
+    
+    const app = express();
+    app.options('*', cors());
+    
+    app.use(express.static(dirname(rootPath)));
+    return app.listen().address().port;
+
+});
 
 /**
  * @method renderToString
- * @param {String} markup
+ * @param {String} rootPath
+ * @param {Boolean} [options = defaultOptions]
  * @return {String}
  */
-export default async function renderToString(markup) {
+export default async function renderToString(rootPath, options = defaultOptions) {
 
-    const browser = await puppeteer.launch({ headless: false });
+    const opts = { ...defaultOptions, ...options };
+    const port = createServer(rootPath);
+    const url = `${opts.protocol}://${opts.hostname}:${port}/${basename(rootPath)}`;
+
+    const browser = await puppeteer.launch({ headless: !opts.debug });
     const page = await browser.newPage();
-    const content = await page.setContent(markup);
-    page.mainFrame().addScriptTag('http://localhost:3000/js/welcome-card/build.js');
 
-    return page.evaluate(() => {
+    await page.goto(url);
 
-        return new Promise(resolve => {
+    const content = await page.evaluate(() => {
+
+        return new Promise(async resolve => {
 
             setTimeout(() => {
                 resolve(document.body.outerHTML);
-            }, 2000);
+            }, 1500);
 
         });
 
     });
+
+    await browser.close();
+    return content;
 
 };
