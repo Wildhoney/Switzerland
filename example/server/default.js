7f40114beede33f08e02c73a6a54f0ef6930c167
Wildhoney <adam.timberlake@gmail.com>
2017-09-22 12:26:04 +0100

    Using rootPath

--- example/server/default.js
+++ example/server/default.js
@@ -1,11 +1,14 @@
+import { dirname } from 'path';
 import express from 'express';
 import renderToString from '../../src/universal';
 
 const app = express();
+const rootPath = __dirname + '/../index.html';
 
 app.get('/', async (request, response) => {
-    const markup = await renderToString(__dirname + '/../index.html');
+    const markup = await renderToString(rootPath);
     response.send(markup);
 });
 
+app.use(express.static(dirname(rootPath)));
 app.listen(3000);
