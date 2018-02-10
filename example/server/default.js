94637cda1f2d0aeecbb3903aece38f2e45e0598a
Wildhoney <adam.timberlake@gmail.com>
2018-02-10 15:56:44 +0000

    Added "Service-Worker-Allowed" header to Express server

--- example/server/default.js
+++ example/server/default.js
@@ -3,5 +3,10 @@ import express from 'express';
 
 const app = express();
 const rootPath = __dirname + '/../index.html';
+app.get('*', (_, response, next) => {
+    response.header('Service-Worker-Allowed', '/');
+    next();
+});
+
 app.use(express.static(dirname(rootPath)));
 app.listen(process.env.PORT || 3000);
