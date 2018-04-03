878cd0cb861ec4c2ccabb3a04b1e0208b63bb314
Wildhoney <adam.timberlake@gmail.com>
2018-04-03 23:27:24 +0100

    Allowed the specifying of the form field names

--- src/utilities.js
+++ src/utilities.js
@@ -1,5 +1,5 @@
 /**
- * @method message ∷ ∀ a. Object String a → Object String a
+ * @method message ∷ ∀ a. Object String a → [String] → Object String a
  * @param {String} message
  * @param {String} type
  * @return {void}
@@ -8,10 +8,10 @@
  * from the generated output if 'NODE_ENV' is defined as 'production', as it will be unused due to 'process.env'
  * checks later on in the code.
  */
-export const validate = event => {
+export const validate = (event, elements = ['input', 'textarea', 'select']) => {
 
     const form = event.path.find(node => node instanceof HTMLFormElement);
-    const fields = Array.from(form.querySelectorAll('input[name]:not(:disabled), textarea[name]:not(:disabled)'));
+    const fields = Array.from(form.querySelectorAll(elements.map(name => `${name}[name]:not(:disabled)`).join(', ')));
     const results = fields.reduce((xs, node) => ({ ...xs, [node.getAttribute('name')]: node.validity }), {});
 
     return { results, valid: Object.values(results).some(({ valid }) => valid) };
