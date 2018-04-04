defe5eb2a4efcc4f6fd7553f4f6d31505004b127
Wildhoney <adam.timberlake@gmail.com>
2018-04-04 18:49:39 +0100

    Changed `some` to `every` for checking the form's validity

--- src/utilities.js
+++ src/utilities.js
@@ -1,22 +1,31 @@
 /**
- * @method message ∷ ∀ a. Object String a → [String] → Object String a
- * @param {String} message
- * @param {String} type
+ * @method validate ∷ ∀ a. Object String a → [String] → Object String a
+ * @param {String} event
+ * @param {String} [nodeNames = ['input', 'textarea', 'select']]
  * @return {void}
  *
  * Takes a message and an optional console type for output. During minification this function will be removed
  * from the generated output if 'NODE_ENV' is defined as 'production', as it will be unused due to 'process.env'
  * checks later on in the code.
  */
-export const validate = (event, elements = ['input', 'textarea', 'select']) => {
+export const validate = (event, nodeNames = ['input', 'textarea', 'select']) => {
 
     try {
 
-        const form = event.path.find(node => node instanceof HTMLFormElement);
-        const fields = Array.from(form.querySelectorAll(elements.map(name => `${name}[name]:not(:disabled)`).join(', ')));
-        const results = fields.reduce((xs, node) => ({ ...xs, [node.getAttribute('name')]: node.validity }), {});
+        // Extend the selector to select only elements that have a `name` attribute and are NOT disabled, as
+        // that's how traiditional forms function: elements without a name and/or disabled won't be submitted.
+        const selector = nodeNames.map(name => `${name}[name]:not(:disabled)`).join(', ');
 
-        return { results, valid: Object.values(results).some(({ valid }) => valid) };
+        // Find the first form that we encounter from the event, and then find all of the related fields using
+        // the above selector.
+        const form = event.path.find(node => node instanceof HTMLFormElement);
+        const fields = Array.from(form.querySelectorAll(selector));
+
+        // Map each of the validity reports into an object where the key is the name of the element.
+        const validityAnalysis = fields.reduce((xs, node) => ({ ...xs, [node.getAttribute('name')]: node.validity }), {});
+        const isFormValid = Object.values(results).some(({ valid }) => valid);
+
+        return { results: validityAnalysis, valid: isFormValid };
 
     } catch (err) {
 
