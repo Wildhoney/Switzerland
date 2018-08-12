1c22fcef6d25ad2fb6053cbb40724049799d3488
Wildhoney <adam.timberlake@gmail.com>
2018-08-12 23:07:02 +0100

    Rearranged props for @switzerland/redux

--- packages/middleware/redux/src/index.js
+++ packages/middleware/redux/src/index.js
@@ -5,21 +5,20 @@ import thunk from 'redux-thunk';
  * @function redux ∷ ∀ a b c d. { (String (a → b), (c → { action: String, payload: d } → c) }
  */
 export default function redux({ actions: actions_, reducer }) {
+    const subscriptions = new WeakSet();
     const store = createStore(reducer, applyMiddleware(thunk));
     const actions = bindActionCreators(actions_, store.dispatch);
-    const subscriptions = new WeakSet();
     const dispatch = store.dispatch;
 
     return props => {
         const state = store.getState();
+        const redux = { state, actions, dispatch };
 
         if (!subscriptions.has(props.node)) {
-            store.subscribe(() =>
-                props.render({ redux: { state, actions, dispatch } })
-            );
+            store.subscribe(() => props.render({ redux }));
             subscriptions.add(props.node);
         }
 
-        return { ...props, redux: { state, actions, dispatch } };
+        return { ...props, redux };
     };
 }
