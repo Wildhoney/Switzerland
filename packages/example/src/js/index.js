bf521ac696b4b61716a64be4a673599df73517a5
Wildhoney <adam.timberlake@gmail.com>
2018-08-12 13:44:07 +0100

    Modified example

--- packages/example/src/js/index.js
+++ packages/example/src/js/index.js
@@ -1,14 +1,16 @@
 import { create } from '@switzerland/core';
 import html from '@switzerland/vdom';
+import attrs from '@switzerland/attrs';
 import styles from './index.css';
 
 create(
-    'x-timedate',
+    'x-welcome',
+    attrs(),
     html(async props => {
         return (
             <section>
                 <style type="text/css">{styles.toString()}</style>
-                <h1>Hello Switzerland!</h1>
+                <h1>Hello {props.attrs.name}!</h1>
                 <time>Time: {Date.now()}</time>
                 <br />
                 <br />
