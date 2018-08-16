eaea6030878451d3b65d1bfd4d55b573ba4bc09a
Wildhoney <adam.timberlake@gmail.com>
2018-08-16 19:56:03 +0100

    Added "delay" middleware

---
+++ src/middleware/delay/index.js
@@ -0,0 +1,15 @@
+/**
+ * @function delay ∷ Props p ⇒ Number → (p → Promise p)
+ * ---
+ * Pauses the middleware processing for the supplied milliseconds.
+ */
+export default function delay(milliseconds) {
+
+    return props => {
+
+        // Use the `setTimeout` to pause the middleware by the given milliseconds.
+        return new Promise(resolve => setTimeout(() => resolve(props), milliseconds));
+
+    };
+
+}
