c775d577400a8916328a50e5c08d2237b5e55a21
Wildhoney <adam.timberlake@gmail.com>
2018-08-16 19:56:55 +0100

    Added "wait" middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,7 +1,9 @@
 import adapt from './adapt/index.js';
 import attrs from './attrs/index.js';
+import delay from './delay/index.js';
 import html from './html/index.js';
 import redux from './redux/index.js';
 import rescue from './rescue/index.js';
+import wait from './wait/index.js';
 
-export default { adapt, attrs, html, redux, rescue };
+export default { adapt, attrs,delay, html, redux, rescue,wait };
---
+++ src/middleware/wait/index.js
@@ -0,0 +1,46 @@
+import { createShadowRoot} from '../html/utils.js';
+import { getEventName } from '../../utils.js';
+
+/**
+ * @function wait ∷ Props p ⇒ [String] → (p → Promise p)
+ *  ---
+ * Takes a list of node names that correspond to Switzerland defined custom elements. Awaits for them to
+ * be mounted in the DOM, including running all of their associated middleware, before resolving the custom element
+ * that the 'wait' middleware was defined on.
+ *
+ * This allows for components to be atomic, in that a parent component cannot be considered resolved until its
+ * child components have been resolved. It could potentially be a long chain of component dependencies.
+ *
+ * It's worth noting that the 'wait' middleware will not await a node that is not in the DOM, therefore it's acceptable
+ * to list nodes that may or may not be in the DOM, depending on conditionals.
+ */
+export default function wait(...names) {
+
+    const eventName = getEventName('resolved');
+
+    return async props => {
+
+        // Determine which elements we need to await being resolved before we continue.
+        const resolved = new Set();
+
+        await new Promise(resolve => {
+
+            // Find all of the nodes to wait upon, minus those that have already been resolved.
+            const nodes = Array.from(names.reduce((accum, name) => ([...accum, ...Array.from(createShadowRoot(props).querySelectorAll(name))]), [])).filter(node => !node.classList.contains('resolved'));
+
+            nodes.length === 0 ? resolve() : addEventListener(eventName, function listener(event) {
+                nodes.includes(event.detail.node) && resolved.add(event.detail.node);
+                if (resolved.size === nodes.length) {
+                    removeEventListener(eventName, listener);
+                    resolve();
+                    resolved.clear();
+                };
+            });
+
+        });
+
+        return props;
+
+    };
+
+}
