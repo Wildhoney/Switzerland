a6c9e6cfd2a4e19e1e69848369ccd1a9eaf07227
Wildhoney <adam.timberlake@gmail.com>
2018-08-17 14:18:49 +0100

    Exporting all middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,22 +1,28 @@
 import adapt from './adapt/index.js';
 import attrs from './attrs/index.js';
-import delay from './delay/index.js';
 import defer from './defer/index.js';
+import delay from './delay/index.js';
 import html from './html/index.js';
 import intersect from './intersect/index.js';
-import redux from './redux/index.js';
+import interval from './interval/index.js';
+import loader from './loader/index.js';
+import methods from './methods/index.js';
 import once from './once/index.js';
+import redux from './redux/index.js';
 import rescue from './rescue/index.js';
 import wait from './wait/index.js';
 
 export default {
     adapt,
     attrs,
-    once,
-    delay,
     defer,
-    intersect,
+    delay,
     html,
+    intersect,
+    interval,
+    loader,
+    methods,
+    once,
     redux,
     rescue,
     wait
