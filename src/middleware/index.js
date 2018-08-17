929603b6a31e4348417dc9a159b6754f4acabe56
Wildhoney <adam.timberlake@gmail.com>
2018-08-17 11:57:55 +0100

    Added "intersect" middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,9 +2,20 @@ import adapt from './adapt/index.js';
 import attrs from './attrs/index.js';
 import delay from './delay/index.js';
 import html from './html/index.js';
+import intersect from './intersect/index.js';
 import redux from './redux/index.js';
 import once from './once/index.js';
 import rescue from './rescue/index.js';
 import wait from './wait/index.js';
 
-export default { adapt, attrs, once, delay, html, redux, rescue, wait };
+export default {
+    adapt,
+    attrs,
+    once,
+    delay,
+    intersect,
+    html,
+    redux,
+    rescue,
+    wait
+};
