599cae3f63a4e5d61d6936b0d678b5429fe556a3
Wildhoney <adam.timberlake@gmail.com>
2018-08-17 14:17:39 +0100

    Added "loader" middleware

---
+++ src/middleware/loader/index.js
@@ -0,0 +1,28 @@
+
+
+/**
+ * @function loader ∷ Props p ⇒ Object String String → (p → Promise p)
+ * ---
+ * Takes an object of image paths and waits for them to be fully downloaded into the browser's cache before
+ * handing over to the subsequent middleware. Appends the "src" object to the props for consumption by the component.
+ */
+export default function loader(sources) {
+
+    return async props => {
+
+        await Promise.all(Object.values(sources).map(src => {
+
+            return new Promise(resolve => {
+                const img = new Image();
+                img.addEventListener('load', resolve);
+                img.addEventListener('error', resolve);
+                img.setAttribute('src', src);
+            });
+
+        }));
+
+        return { ...props, src: sources };
+
+    };
+
+}
