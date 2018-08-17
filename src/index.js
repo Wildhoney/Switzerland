a620073a1a7e83e7288d3d52b9979e81964365ce
Wildhoney <adam.timberlake@gmail.com>
2018-08-17 11:58:51 +0100

    Reintrocued the queuing system for rendering

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ const previous = new WeakMap();
 export { u, m, h };
 
 /**
+ * @constant queue ∷ Symbol
+ */
+const queue = Symbol('@switzerland/queue');
+
+/**
  * @function init ∷ View v, String s ⇒ Object s s → Object { path: s → s, stylesheet: (s → s) → s → v }
  * ---
  * Initialisation function that takes the meta data of the component file (usually via `import.meta`) and
@@ -40,6 +45,11 @@ export function create(name, ...middleware) {
         tag,
         class extends prototype {
             /**
+             * @constant ∷ Symbol
+             */
+            [queue] = new Set();
+
+            /**
              * @method connectedCallback ∷ Props p ⇒ p
              */
             connectedCallback() {
@@ -58,52 +68,90 @@ export function create(name, ...middleware) {
              * @method render ∷ ∀ a. Props p ⇒ Object String a → p
              */
             async render(mergeProps = {}) {
-                const prevProps = previous.get(this);
-                const dispatchEvent = u.dispatchEvent(this);
+                const scheduledTask = new Promise(async resolve => {
+                    // Await the completion of the task last added to the stack.
+                    const tasks = Array.from(this[queue]);
+                    const prevScheduledTask = tasks[tasks.length - 1];
+                    await prevScheduledTask;
 
-                const initialProps = {
-                    ...(prevProps || {}),
-                    ...mergeProps,
-                    node: this,
-                    render: this.render.bind(this),
-                    dispatch: dispatchEvent,
-                    prevProps: previous.get(this) || null
-                };
+                    const prevProps = previous.get(this);
+                    const dispatchEvent = u.dispatchEvent(this);
+                    const isResolved = async () => {
+                        const resolution = await Promise.race([
+                            scheduledTask,
+                            Promise.resolve(false)
+                        ]);
+                        return resolution !== false;
+                    };
 
-                try {
-                    const props = await middleware.reduce(
-                        async (accumP, middleware) => {
-                            const props = middleware({
-                                ...(await accumP),
-                                props: await accumP
-                            });
+                    const initialProps = {
+                        ...(prevProps || {}),
+                        ...mergeProps,
+                        isResolved,
+                        node: this,
+                        render: this.render.bind(this),
+                        dispatch: dispatchEvent,
+                        prevProps: previous.get(this) || null
+                    };
 
-                            // Determine if there's an error handler in the current set of props. If there is then
-                            // set the handler function as the default to be used if an error is subsequently thrown.
-                            handler in props && handlers.set(this, props);
-                            return props;
-                        },
-                        initialProps
-                    );
-
-                    previous.set(this, props);
-                    return props;
-                } catch (error) {
-                    // Attempt to find an error handler for the current node which can handle the error gracefully.
-                    // Otherwise a simple yet abrasive `console.error` will be used with no recovery possible.
-                    const props = handlers.get(this);
-
-                    if (!props) {
-                        console.error(error);
+                    if (
+                        prevScheduledTask &&
+                        !this[queue].has(prevScheduledTask)
+                    ) {
+                        // If a caught error has removed it from the queue, then we don't go any further.
+                        resolve();
                         return;
                     }
 
-                    previous.set(this, { ...props, error });
-                    props[handler]({ ...props, error });
-                } finally {
-                    dispatchEvent(u.getEventName('resolved'), { node: this });
-                    this.isConnected && this.classList.add('resolved');
-                }
+                    try {
+                        const props = await middleware.reduce(
+                            async (accumP, middleware) => {
+                                const props = await accumP;
+                                const newProps = middleware({
+                                    ...props,
+                                    props
+                                });
+
+                                // Determine if there's an error handler in the current set of props. If there is then
+                                // set the handler function as the default to be used if an error is subsequently thrown.
+                                handler in newProps &&
+                                    handlers.set(this, newProps);
+                                return newProps;
+                            },
+                            initialProps
+                        );
+
+                        previous.set(this, props);
+                        return props;
+                    } catch (error) {
+                        // Attempt to find an error handler for the current node which can handle the error gracefully.
+                        // Otherwise a simple yet abrasive `console.error` will be used with no recovery possible.
+                        const props = handlers.get(this);
+
+                        if (!props) {
+                            console.error(error);
+                            return;
+                        }
+
+                        previous.set(this, { ...props, error });
+                        props[handler]({ ...props, error });
+                    } finally {
+                        // Finally dispatch the event for parent components to be able to resolve, and add
+                        // the "resolved" class to the element.
+                        dispatchEvent(u.getEventName('resolved'), {
+                            node: this
+                        });
+                        this.isConnected && this.classList.add('resolved');
+
+                        // Task has been completed successfully.
+                        this[queue].delete(prevScheduledTask);
+                        resolve();
+                    }
+                });
+
+                // Add task to the queue.
+                this[queue].add(scheduledTask);
+                return scheduledTask;
             }
         }
     );
