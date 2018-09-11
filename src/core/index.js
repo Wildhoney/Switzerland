0de05909fd40867f023554c4422c279ea9e8b100
Wildhoney <adam.timberlake@gmail.com>
2018-09-11 12:39:25 +0100

    Improved `alias` function. Fixes #35

--- src/core/index.js
+++ src/core/index.js
@@ -5,7 +5,6 @@ import * as t from './types.js';
 
 export const handlers = new WeakMap();
 export const previous = new WeakMap();
-const middlewares = new WeakMap();
 
 export { m, h, t };
 
@@ -25,113 +24,139 @@ export const init = url => path => new URL(path, url).pathname;
  */
 export function create(name, ...middleware) {
     const parts = name.split('/');
-    const [tag, prototype] = [
+    const [tag, Prototype] = [
         u.resolveTagName(parts[0]),
         u.getPrototype(parts[1])
     ];
 
-    const constructor = class extends prototype {
-        constructor() {
-            super();
-            this[queue] = new Set();
-            this[state] = 'normal';
-        }
-
-        /**
-         * @method connectedCallback ∷ Props p ⇒ p
-         */
-        connectedCallback() {
-            return this.render();
-        }
-
-        /**
-         * @method disconnectedCallback ∷ Props p ⇒ p
-         */
-        disconnectedCallback() {
-            this.classList.remove('resolved');
-            return this.render();
-        }
-
-        /**
-         * @method render ∷ ∀ a. Props p ⇒ Object String a → p
-         */
-        async render(mergeProps = {}) {
-            if (this[state] === 'error') {
-                // Cannot process new render cycles until the error has been resolved.
-                return;
+    customElements.define(
+        tag,
+        class extends Prototype {
+            constructor() {
+                super();
+                this[queue] = new Set();
+                this[state] = 'normal';
             }
 
-            const scheduledTask = new Promise(async resolve => {
-                // Await the completion of the task last added to the stack.
-                const tasks = Array.from(this[queue]);
-                const prevScheduledTask = tasks[tasks.length - 1];
-                await prevScheduledTask;
+            /**
+             * @method connectedCallback ∷ Props p ⇒ p
+             */
+            connectedCallback() {
+                return this.render();
+            }
 
-                const dispatchEvent = u.dispatchEvent(this);
-                const initialProps = u.getInitialProps(
-                    this,
-                    mergeProps,
-                    scheduledTask
-                );
+            /**
+             * @method disconnectedCallback ∷ Props p ⇒ p
+             */
+            disconnectedCallback() {
+                this.classList.remove('resolved');
+                return this.render();
+            }
 
-                if (prevScheduledTask && !this[queue].has(prevScheduledTask)) {
-                    // If a caught error has removed it from the queue, then we don't go any further.
-                    resolve();
+            /**
+             * @method render ∷ ∀ a. Props p ⇒ Object String a → p
+             */
+            async render(mergeProps = {}) {
+                if (this[state] === 'error') {
+                    // Cannot process new render cycles until the error has been resolved.
                     return;
                 }
 
-                try {
-                    // Cycle through all of the middleware functions, updating the props as we go.
-                    return await u.processMiddleware(
+                const scheduledTask = new Promise(async resolve => {
+                    // Await the completion of the task last added to the stack.
+                    const tasks = Array.from(this[queue]);
+                    const prevScheduledTask = tasks[tasks.length - 1];
+                    await prevScheduledTask;
+
+                    const dispatchEvent = u.dispatchEvent(this);
+                    const initialProps = u.getInitialProps(
                         this,
-                        initialProps,
-                        middleware
+                        mergeProps,
+                        scheduledTask
                     );
-                } catch (error) {
-                    // Errors should cancel any enqueued middleware.
-                    this[queue].clear();
-                    this[state] = 'error';
 
-                    // Handle any errors that were thrown from the processing of the middleware functions.
-                    return u.handleError(this, error);
-                } finally {
-                    // Await the resolution of all the CSS import rules.
-                    const stylesheets = u
-                        .createShadowRoot(this)
-                        .querySelectorAll('style');
-                    await u.hasLoadedCSSImports(stylesheets);
+                    if (
+                        prevScheduledTask &&
+                        !this[queue].has(prevScheduledTask)
+                    ) {
+                        // If a caught error has removed it from the queue, then we don't go any further.
+                        resolve();
+                        return;
+                    }
 
-                    // Always dispatch the "resolved" event regardless of success or failure. We also apply
-                    // the "resolved" class name to the element.
-                    dispatchEvent(u.getEventName('resolved'), {
-                        node: this
-                    });
-                    this.isConnected && this.classList.add('resolved');
-                    this[queue].delete(prevScheduledTask);
-                    resolve();
-                }
-            });
+                    try {
+                        // Cycle through all of the middleware functions, updating the props as we go.
+                        return await u.processMiddleware(
+                            this,
+                            initialProps,
+                            middleware
+                        );
+                    } catch (error) {
+                        // Errors should cancel any enqueued middleware.
+                        this[queue].clear();
+                        this[state] = 'error';
 
-            // Add task to the queue.
-            this[queue].add(scheduledTask);
-            return scheduledTask;
+                        // Handle any errors that were thrown from the processing of the middleware functions.
+                        return u.handleError(this, error);
+                    } finally {
+                        // Await the resolution of all the CSS import rules.
+                        const stylesheets = u
+                            .createShadowRoot(this)
+                            .querySelectorAll('style');
+                        await u.hasLoadedCSSImports(stylesheets);
+
+                        // Always dispatch the "resolved" event regardless of success or failure. We also apply
+                        // the "resolved" class name to the element.
+                        dispatchEvent(u.getEventName('resolved'), {
+                            node: this
+                        });
+                        this.isConnected && this.classList.add('resolved');
+                        this[queue].delete(prevScheduledTask);
+                        resolve();
+                    }
+                });
+
+                // Add task to the queue.
+                this[queue].add(scheduledTask);
+                return scheduledTask;
+            }
         }
-    };
+    );
 
-    customElements.define(tag, constructor);
-
-    middlewares.set(constructor, middleware);
     return tag;
 }
 
 /**
  * @function alias ∷ String → String → String
- * ---
- * Takes the name of the web component and an array of functions that represent the middleware. Each
- * middleware item takes in the accumulated props, and yields props to pass to the next item in the list.
  */
 export function alias(name, newName) {
-    const constructor = customElements.get(name);
-    const middleware = middlewares.get(constructor);
-    return create(newName, ...middleware);
+    const Constructor = customElements.get(name);
+    const instance = new Constructor();
+    const parts = newName.split('/');
+    const Prototype = u.getPrototype(parts[1]);
+
+    customElements.define(
+        newName,
+        class extends Prototype {
+            constructor() {
+                super();
+                this[queue] = new Set();
+                this[state] = 'normal';
+            }
+
+            connectedCallback() {
+                return instance.connectedCallback.call(this);
+            }
+
+            disconnectedCallback() {
+                return instance.disconnectedCallback.call(this);
+            }
+
+            render() {
+                return instance.render.call(this);
+            }
+        }
+    );
+
+    return newName;
 }
