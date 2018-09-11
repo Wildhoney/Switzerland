99bd8d4041f0e730c5a7d14c12f0e2cdc690cfd4
Wildhoney <adam.timberlake@gmail.com>
2018-09-11 22:21:06 +0100

    Fixed bug in the `once` middleware

--- src/middleware/once/index.js
+++ src/middleware/once/index.js
@@ -8,6 +8,11 @@ export const types = {
 };
 
 /**
+ * @constant caches ∷ Map
+ */
+const caches = new Map();
+
+/**
  * @function once ∷ Props p ⇒ (p → p) → Symbol → (p → Promise p)
  * ---
  * Takes a function and an optional strategy that allows for the passed function to be invoked only once per instance
@@ -24,18 +29,14 @@ export const types = {
  * to be invoked multiple times, but not on a simple refresh of the component's contents.
  */
 export default function once(fn, strategy = types.ONLY) {
-    const caches = new Map();
-    caches.set(fn, new Set());
-    const cache = caches.get(fn);
 
-    /**
-     * @function maybeInvoke
-     */
-    function maybeInvoke(fn, props) {
+    !caches.has(fn) && caches.set(fn, new Set());
+    const cache = caches.get(fn);
+    const maybeInvoke = (fn, props)=> {
         if (cache.has(props.node)) {
             return props;
         }
-
+        
         const result = fn(props);
         cache.add(props.node);
         return result;
@@ -45,12 +46,12 @@ export default function once(fn, strategy = types.ONLY) {
         if (props.node.isConnected) {
             const result =
                 strategy !== types.ON_UNMOUNT && (await maybeInvoke(fn, props));
-            strategy === types.ON_UNMOUNT && cache.delete(props.node);
+            // strategy === types.ON_UNMOUNT && cache.delete(props.node);
             return { ...result, ...props };
         }
 
         const result = await maybeInvoke(fn, props);
-        strategy === types.ON_MOUNT && cache.delete(props.node);
+        // strategy === types.ON_MOUNT && cache.delete(props.node);
         return { ...result, ...props };
     };
 }
