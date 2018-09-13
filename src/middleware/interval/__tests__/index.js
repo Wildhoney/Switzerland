c3a1e10e6eee1c15b1739e2b950b3852545b351e
Wildhoney <adam.timberlake@gmail.com>
2018-09-13 19:32:05 +0100

    Allowed more latitude for the timings in the 'interval' tests

--- src/middleware/interval/__tests__/index.js
+++ src/middleware/interval/__tests__/index.js
@@ -9,14 +9,14 @@ test('It should invoke `render` every 10 milliseconds until unmounted;', async t
     const [mountM, unmountM] = interval(10);
     const newMountProps = mountM({ ...defaultProps, render });
     t.is(render.callCount, 0);
-    await delay(11);
+    await delay(15);
     t.is(render.callCount, 1);
-    await delay(11);
+    await delay(10);
     t.is(render.callCount, 2);
     await delay(1);
     t.is(render.callCount, 2);
     const newUnmountProps = unmountM(defaultProps);
-    await delay(27);
+    await delay(24);
     t.is(render.callCount, 2);
     t.deepEqual(await newMountProps, { ...defaultProps, render });
     t.deepEqual(await newUnmountProps, defaultProps);
