7c84ed7ac0bc248591b5ac83d64b2a0736a0a17b
Wildhoney <adam.timberlake@gmail.com>
2018-09-15 12:35:51 +0100

    Added service worker middleware

---
+++ src/middleware/service-worker/index.js
@@ -0,0 +1,21 @@
+import { once } from '../index.js';
+
+/**
+ * @function serviceWorker ∷ Props p ⇒ String → String → (p → Promise p)
+ */
+export default function serviceWorker(type, path, scope) {
+    return once(async props => {
+        try {
+            switch (type) {
+                case 'service':
+                    navigator.serviceWorker &&
+                        (await navigator.serviceWorker.register(path, {
+                            scope
+                        }));
+                    return props;
+            }
+        } catch (err) {
+            return props;
+        }
+    });
+}
