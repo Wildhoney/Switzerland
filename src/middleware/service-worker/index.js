2860e9de683810d24cff1702fac205f0c5d1089d
Wildhoney <adam.timberlake@gmail.com>
2018-09-16 13:35:45 +0100

    Removed "type" from service worker middleware

--- src/middleware/service-worker/index.js
+++ src/middleware/service-worker/index.js
@@ -3,17 +3,14 @@ import once from '../once/index.js';
 /**
  * @function serviceWorker ∷ Props p ⇒ String → String → (p → Promise p)
  */
-export default function serviceWorker(type, path, scope) {
+export default function serviceWorker(path, scope) {
     return once(async props => {
         try {
-            switch (type) {
-                case 'service':
-                    navigator.serviceWorker &&
-                        (await navigator.serviceWorker.register(path, {
-                            scope
-                        }));
-                    return props;
-            }
+            navigator.serviceWorker &&
+                (await navigator.serviceWorker.register(path, {
+                    scope
+                }));
+            return props;
         } catch (err) {
             return props;
         }
