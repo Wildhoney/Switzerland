87f7689d630a5b17e580ec9307daba42d6b9e63a
Wildhoney <adam.timberlake@gmail.com>
2018-09-17 21:48:00 +0100

    Modified comment for function decl

--- src/middleware/defer/index.js
+++ src/middleware/defer/index.js
@@ -1,5 +1,5 @@
 /**
- * @method function ∷ Props p ⇒ (p → p) → Number → (p → p)
+ * @method function ∷ Props p ⇒ (Promise p → p) → Number → (p → p)
  * ---
  * Invokes the function (likely a middleware function) after X milliseconds, unless the current render cycle
  * has been completed. It's useful for such things as only rendering a "Loading" message if the component is
