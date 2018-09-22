db4ab8fe037754b00120536f677bc55fd1dd4930
Wildhoney <adam.timberlake@gmail.com>
2018-09-22 14:41:51 +0100

    Added cors to the example

--- example/server/index.mjs
+++ example/server/index.mjs
@@ -1,8 +1,10 @@
 import path from 'path';
 import express from 'express';
+import cors from 'cors';
 import compression from 'compression';
 
 const app = express();
+app.use(cors());
 app.use(compression());
 
 const example = path.resolve('./example/app');
