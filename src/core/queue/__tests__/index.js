955d9496d75ddd4ff55e7e1d3bf4b835cb7faf85
Wildhoney <adam.timberlake@gmail.com>
2018-10-18 22:42:19 +0100

    Prettier?

--- src/core/queue/__tests__/index.js
+++ src/core/queue/__tests__/index.js
@@ -7,7 +7,7 @@ test('It should be able to determine the current task;', t => {
     const queue = createQueue();
     queue.push(mockTask);
     t.is(queue.current(), mockTask);
-    [1, 2, 3].forEach(() => queue.push(mockTask))
+    [1, 2, 3].forEach(() => queue.push(mockTask));
     t.is(queue.current(), mockTask);
 });
 
@@ -17,15 +17,15 @@ test('It should be able to drop tasks;', t => {
     t.is(queue.current(), mockTask);
     queue.drop(mockTask);
     t.true(queue.isEmpty());
-    [1, 2, 3].forEach(() => queue.push(mockTask))
+    [1, 2, 3].forEach(() => queue.push(mockTask));
     t.false(queue.isEmpty());
     queue.dropAll();
     t.true(queue.isEmpty());
 });
 
-test('It should be able to determine if the current task has become invalid;', t=>{
+test('It should be able to determine if the current task has become invalid;', t => {
     const queue = createQueue();
     t.true(queue.isInvalid(mockTask));
     queue.push(mockTask);
     t.false(queue.isInvalid(mockTask));
-})
+});
--- src/core/state/__tests__/index.js
+++ src/core/state/__tests__/index.js
@@ -1,10 +1,10 @@
 import test from 'ava';
 import createState from '../index.js';
 
-const mockNode = document.createElement('mock-element')
+const mockNode = document.createElement('mock-element');
 
 test('It should be able to determine the state;', t => {
-    const state  = createState(mockNode);
+    const state = createState(mockNode);
     t.false(state.isError());
     state.setError();
     t.true(state.isError());
