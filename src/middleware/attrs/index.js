25fa7764c4ba47fca136a15e808601cbbc8b47cd
Wildhoney <adam.timberlake@gmail.com>
2018-10-20 14:14:17 +0100

    Refactored out routines into util functions for the 'attrs' middleware

--- src/middleware/attrs/index.js
+++ src/middleware/attrs/index.js
@@ -13,30 +13,15 @@ import * as u from './utils.js';
  */
 export default function attrs(types = {}, exclude = ['class', 'id', 'style']) {
     const observers = new Map();
-    const defaults = Object.entries(types).reduce(
-        (accum, [key, value]) =>
-            Array.isArray(value) && typeof value[1] !== 'undefined'
-                ? { ...accum, [key]: value[1] }
-                : accum,
-        {}
-    );
+    const defaults = u.getDefaults(types);
 
     return props => {
         if (!observers.has(props.node)) {
-            const observer = new window.MutationObserver(mutations => {
-                mutations.some(mutation => {
-                    // Only cause a re-render if some of the mutated items have actually changed the attribute
-                    // when compared, and are not included in the `exclude` list specified in the function's
-                    // parameters.
-                    const isObserved = !exclude.includes(
-                        mutation.attributeName
-                    );
-                    const isModified =
-                        mutation.oldValue !==
-                        props.node.getAttribute(mutation.attributeName);
-                    return isObserved && isModified;
-                }) && props.render();
-            });
+            const observer = new window.MutationObserver(
+                mutations =>
+                    u.hasApplicableMutations(props.node, mutations, exclude) &&
+                    props.render()
+            );
 
             observer.observe(props.node, {
                 attributes: true,
@@ -45,19 +30,9 @@ export default function attrs(types = {}, exclude = ['class', 'id', 'style']) {
             observers.set(props.node, observer);
         }
 
-        const attrs = Object.values(props.node.attributes).reduce(
-            (acc, attr) => {
-                const name = u.kebabToCamel(attr.nodeName);
-                const [f] = [].concat(types[name] || (a => a));
-
-                return {
-                    ...acc,
-                    [name]: f(attr.nodeValue)
-                };
-            },
-            defaults
-        );
-
-        return { ...props, attrs };
+        return {
+            ...props,
+            attrs: u.transformAttributes(props.node.attributes, defaults, types)
+        };
     };
 }
--- src/middleware/attrs/utils.js
+++ src/middleware/attrs/utils.js
@@ -1,8 +1,46 @@
 /**
- * @method kebabToCamel ∷ String → String
- * @param {String} value
- * @return {String}
+ * @function kebabToCamel ∷ String → String
  */
 export const kebabToCamel = value => {
     return value.replace(/(-\w)/g, match => match[1].toUpperCase());
 };
+
+/**
+ * @function getDefaults ∷ ∀ a. Object String (String → a)
+ */
+export const getDefaults = types =>
+    Object.entries(types).reduce(
+        (accum, [key, value]) =>
+            Array.isArray(value) && typeof value[1] !== 'undefined'
+                ? { ...accum, [key]: value[1] }
+                : accum,
+        {}
+    );
+
+/**
+ * @function transformAttributes ∷ ∀ a b c. NamedNodeMap mnm, Object o ⇒ mnm -> o String a -> o String (String → b) -> o (String → c)
+ */
+export const transformAttributes = (attrs, defaults, types) =>
+    Object.values(attrs).reduce((acc, attr) => {
+        const name = kebabToCamel(attr.nodeName);
+        const [f] = [].concat(types[name] || (a => a));
+
+        return {
+            ...acc,
+            [name]: f(attr.nodeValue)
+        };
+    }, defaults);
+
+/**
+ * @function hasApplicableMutations ∷ HTMLElement e, MutationRecord mr ⇒ e -> mr -> [String] -> Boolean
+ * ---
+ * Only cause a re-render if some of the mutated items have actually changed the attribute when compared, and
+ * are not included in the `exclude` list specified in the function's parameters.
+ */
+export const hasApplicableMutations = (node, mutations, exclude) =>
+    mutations.some(mutation => {
+        const isObserved = !exclude.includes(mutation.attributeName);
+        const isModified =
+            mutation.oldValue !== node.getAttribute(mutation.attributeName);
+        return isObserved && isModified;
+    });
