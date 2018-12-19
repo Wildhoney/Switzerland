613778491d6c7956a2e0630accf7e92a7a50dc4f
Wildhoney <adam.timberlake@gmail.com>
2018-12-19 19:50:02 +0000

    Show appropriate message when todos all filtered out

--- example/app/nodes/todo-list/index.js
+++ example/app/nodes/todo-list/index.js
@@ -5,38 +5,47 @@ const path = init(import.meta.url);
 
 const container = ({ redux, h, props }) =>
     h('ul', {}, [
-        list(props),
+        !!redux.state.list.length && list(props),
         !redux.state.list.length && nothing(props),
         h.stylesheet(path('styles/index.css')),
         h.stylesheet(path('styles/mobile.css'), '(max-width: 768px)'),
         h.stylesheet(path('styles/print.css'), 'print')
     ]);
 
-const list = ({ history, redux, h }) =>
-    redux.state.list
+const list = ({ history, redux, h, props }) => {
+    const todos = redux.state.list
         .filter(model => (history.params.get('show_done') ? true : !model.done))
-        .sort((a, b) => a.created - b.created)
-        .map(model =>
-            h('li', { class: model.done ? 'done' : '' }, [
-                h(
-                    'p',
-                    { onclick: () => redux.actions.mark(model.id) },
-                    model.text
-                ),
-                h(
-                    'button',
-                    {
-                        class: 'delete',
-                        onclick: () => redux.actions.remove(model.id)
-                    },
-                    'Delete'
-                )
-            ])
-        );
+        .sort((a, b) => a.created - b.created);
+
+    return todos.length === 0
+        ? filtered(props)
+        : todos.map(model =>
+              h('li', { class: model.done ? 'done' : '' }, [
+                  h(
+                      'p',
+                      { onclick: () => redux.actions.mark(model.id) },
+                      model.text
+                  ),
+                  h(
+                      'button',
+                      {
+                          class: 'delete',
+                          onclick: () => redux.actions.remove(model.id)
+                      },
+                      'Delete'
+                  )
+              ])
+          );
+};
 
 const nothing = ({ h }) =>
     h('li', { class: 'none' }, [
         h('p', {}, 'You have not added any todos yet.')
+    ]);
+
+const filtered = ({ history, h }) =>
+    h('li', { class: 'none' }, [
+        h('p', {}, 'You are filtering out all completed todos.')
     ]);
 
 export default create(
