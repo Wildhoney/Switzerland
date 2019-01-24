30f092d11e7212bf1622534d763a1a1ef14f0898
Wildhoney <adam.timberlake@gmail.com>
2019-01-24 22:30:27 +0000

    Prettier formatting

--- bin/index.mjs
+++ bin/index.mjs
@@ -8,18 +8,19 @@ import capitalise from 'capitalize';
 import makeDir from 'mkdirp';
 import copyDir from 'copy-dir';
 import glob from 'glob';
-import fmt from "string-template"
-import * as theme from "./theme.mjs"
+import fmt from 'string-template';
+import * as theme from './theme.mjs';
 
 const cwd = path.dirname(new URL(import.meta.url).pathname);
 const pkg = JSON.parse(
     fs.readFileSync(path.resolve(`${cwd}/../package.json`), 'utf8')
-    );
-    const sourceDir = path.resolve(cwd, 'template');
-    const targetDir = path.resolve(cwd, '..', process.argv[2]);
-    const model = {
-        name: path.basename(process.argv[2]),version: pkg.version
-    }
+);
+const sourceDir = path.resolve(cwd, 'template');
+const targetDir = path.resolve(cwd, '..', process.argv[2]);
+const model = {
+    name: path.basename(process.argv[2]),
+    version: pkg.version
+};
 
 async function main() {
     figlet(capitalise(pkg.name), { font: 'univers' }, (_, data) => {
@@ -34,8 +35,8 @@ async function main() {
         makeDir(targetDir, () => {
             copyDir.sync(sourceDir, targetDir);
             glob.sync(`${targetDir}/**/*.{js,css}`).forEach(file => {
-                const content = fmt(fs.readFileSync(file, "utf8"), model);
-                fs.writeFileSync(file, content)
+                const content = fmt(fs.readFileSync(file, 'utf8'), model);
+                fs.writeFileSync(file, content);
             });
 
             console.log(
@@ -44,7 +45,6 @@ async function main() {
                 theme.message(model.name),
                 '\n'
             );
-
         });
     });
 }
