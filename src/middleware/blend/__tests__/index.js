257e37fe7347a5a225cc0a70b91de8937c936638
Wildhoney <adam.timberlake@gmail.com>
2019-03-04 20:45:30 +0000

    Added test for `blend` generator fn support

--- src/middleware/blend/__tests__/index.js
+++ src/middleware/blend/__tests__/index.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import defaultProps from '../../../../tests/helpers/default-props.js';
 import blend from '../index.js';
 
-test('It should be able to invoke the render once the promise has been resolved;', async t => {
+test('It should be able to invoke the `render` function for promises;', async t => {
     const promise = () =>
         new Promise(resolve => {
             resolve({ name: 'Adam' });
@@ -13,6 +13,21 @@ test('It should be able to invoke the render once the promise has been resolved;
 
     t.is(defaultProps.render.callCount, 0);
     await promise;
+    t.is(defaultProps.render.callCount, 1);
+    t.true(defaultProps.render.calledWith({ name: 'Adam' }));
+});
+
+test('It should be able to invoke the `render` function for observables;', async t => {
+    const generator = async function*() {
+        yield { name: 'Adam' };
+    };
+
+    const m = blend(generator);
+    const newProps = m({ ...defaultProps, name: 'Maria' });
+    t.is(defaultProps.render.callCount, 0);
+
+    await newProps;
+
     t.is(defaultProps.render.callCount, 1);
     t.true(defaultProps.render.calledWith({ name: 'Adam' }));
 });
