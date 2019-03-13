98c16e99e780db40940e83eaad7f70b7eb92659a
Wildhoney <adam.timberlake@gmail.com>
2019-03-13 22:06:09 +0000

    Renamed `CancelError` to `Cancel`

--- src/core/index.js
+++ src/core/index.js
@@ -1,7 +1,7 @@
 import * as u from './utils.js';
 import * as impl from './impl/index.js';
 
-export { meta, CancelError } from './utils.js';
+export { meta, Cancel } from './utils.js';
 
 /**
  * @function init ∷ String → String → (String → String)
