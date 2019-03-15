89c2c29bfdc311a220f2a5286b4c21063c642214
Wildhoney <adam.timberlake@gmail.com>
2019-03-15 11:06:21 +0000

    Removed utils as it doesn't provide anything extra

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import * as t from './types/index.js';
 import * as m from './middleware/index.js';
-import * as u from './utils/index.js';
 
 export { create, alias, init } from './core/index.js';
-export { t, m, u };
+export { t, m };
