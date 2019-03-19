10eeea85c078f96fb0f1e2c28170ab94f3f97b3f
Wildhoney <adam.timberlake@gmail.com>
2019-03-19 12:00:04 +0000

    Added Redux tests. Fixes #64

---
+++ src/middleware/redux/__tests__/index.js
@@ -0,0 +1,63 @@
+import test from 'ava';
+import { spy, match } from 'sinon';
+import * as R from 'ramda';
+import defaultProps from '../../../../tests/helpers/default-props.js';
+import redux, { nodes } from '../';
+
+test.beforeEach(t => {
+    t.context.reducer = spy();
+    t.context.actions = { test: R.identity };
+});
+
+test.afterEach(() => {
+    defaultProps.render.resetHistory();
+    nodes.delete(defaultProps.node);
+});
+
+test('It should be able to bind the actions to the dispatch;', t => {
+    const { reducer, actions } = t.context;
+    const m = redux(reducer, actions);
+    const newProps = m(defaultProps);
+
+    t.is(reducer.callCount, 1);
+    newProps.redux.actions.test({ type: 'test' });
+    t.is(reducer.callCount, 2);
+    t.true(reducer.calledWith(match.any, { type: 'test' }));
+});
+
+test.serial(
+    'It should be able to invoke the `render` function on update;',
+    t => {
+        const { reducer, actions } = t.context;
+        const m = redux(reducer, actions);
+        const newProps = m(defaultProps);
+
+        t.is(defaultProps.render.callCount, 0);
+        newProps.redux.actions.test({ type: 'test' });
+        t.is(defaultProps.render.callCount, 1);
+    }
+);
+
+test('It should be able to yield the necessary props;', t => {
+    const { reducer, actions } = t.context;
+    const m = redux(reducer, actions);
+    const newProps = m(defaultProps);
+
+    t.deepEqual(newProps, { ...defaultProps, redux: newProps.redux });
+});
+
+test.serial(
+    'It should only subscribe once to the updates per node instance;',
+    t => {
+        nodes.add = spy(nodes.add);
+
+        for (let index = 0; index <= 3; index++) {
+            const { reducer, actions } = t.context;
+            const m = redux(reducer, actions);
+            m(defaultProps);
+        }
+
+        t.is(nodes.add.callCount, 1);
+        t.true(nodes.has(defaultProps.node));
+    }
+);
--- src/middleware/redux/index.js
+++ src/middleware/redux/index.js
@@ -2,10 +2,14 @@ import * as r from 'https://cdn.jsdelivr.net/npm/@wildhoney/redux@4.0.1/es/redux
 import thunk from 'https://cdn.jsdelivr.net/npm/redux-thunk@2.3.0/es/index.js';
 
 /**
+ * @constant nodes ∷ WeakSet
+ */
+export const nodes = new WeakSet();
+
+/**
  * @function redux ∷ ∀ a b c d. (a → Object String b) → Object String c → d
  */
 export default function redux(reducer, actions) {
-    const subscriptions = new WeakSet();
     const { dispatch, getState, subscribe } = r.createStore(
         reducer,
         r.applyMiddleware(thunk)
@@ -16,9 +20,9 @@ export default function redux(reducer, actions) {
         const state = getState();
         const redux = { state, actions: actions_, dispatch };
 
-        if (!subscriptions.has(props.node)) {
+        if (!nodes.has(props.node)) {
             subscribe(() => props.render({ redux }));
-            subscriptions.add(props.node);
+            nodes.add(props.node);
         }
 
         return { ...props, redux };
