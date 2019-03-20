6ba4c98305fcf638ee14f4b658deec1115f08cd7
Wildhoney <adam.timberlake@gmail.com>
2019-03-20 21:55:44 +0000

    Added tests for html utils

--- src/middleware/html/utils.js
+++ src/middleware/html/utils.js
@@ -7,23 +7,17 @@ export const styles = new WeakMap();
 /**
  * @function putTree ∷ HTMLElement e, View v ⇒ e → v → void
  */
-export const putTree = (node, view) => {
-    trees.set(node, view);
-};
+export const putTree = (node, view) => void trees.set(node, view);
 
 /**
  * @function takeTree ∷ HTMLElement e, ShadowRoot s ⇒ e → s|e
  */
-export const takeTree = node => {
-    return trees.get(node);
-};
+export const takeTree = node => trees.get(node);
 
 /**
  * @function toKebab ∷ String → String
  */
-const toKebab = value => {
-    return value.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
-};
+export const toKebab = value => value.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
 
 /**
  * @function sheet ∷ ∀ a. HTMLElement e, View v ⇒ e → String → String → v → Object String a
