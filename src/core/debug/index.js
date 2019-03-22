cf16008f792a2b411915301bbd5dde263a700a02
Wildhoney <adam.timberlake@gmail.com>
2019-03-22 16:30:48 +0000

    Debug

--- src/core/debug/index.js
+++ src/core/debug/index.js
@@ -7,9 +7,9 @@ export const options = new Map([['enabled', false], ['threshold', -Infinity]]);
  * @function setup ∷ void → void
  */
 export const setup = () => {
-    window.swiss = {
-        enable: () => void options.set('enabled', true),
-        disable: () => void options.set('enabled', false),
-        threshold: value => void options.set('threshold', value)
-    };
+        window.swiss = {
+            enable: () => void options.set('enabled', true),
+            disable: () => void options.set('enabled', false),
+            threshold: value => void options.set('threshold', value)
+        };
 };
