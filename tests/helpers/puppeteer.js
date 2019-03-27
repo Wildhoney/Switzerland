74754e8df3c4aaa42f1649c02855ad43b341caf7
Wildhoney <adam.timberlake@gmail.com>
2019-03-27 10:08:47 +0000

    Updated Puppeteer to wait for custom element definition

--- tests/helpers/puppeteer.js
+++ tests/helpers/puppeteer.js
@@ -1,11 +1,23 @@
 import puppeteer from 'puppeteer';
+import * as R from "ramda"
+
+const append = R.curry(async (page, name) => {
+    await page.waitForFunction(`Boolean(customElements.get('${name}'))`);
+
+    return page.evaluate(async (name) => {
+        const node = document.createElement(name);
+        document.body.appendChild(node);
+        await node.render();
+        return node.innerHTML;
+    }, name);
+})
 
 export default async function withPage(t, run) {
-    const browser = await puppeteer.launch({ slowMo: 500 });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     try {
-        await run(t, page);
+        await run({t, page, append: append(page)});
     } finally {
         await page.close();
         await browser.close();
