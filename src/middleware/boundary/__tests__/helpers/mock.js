cac7cf42fb5bf21e76835dba2f4f0c0580608c7d
Wildhoney <adam.timberlake@gmail.com>
2019-04-01 12:02:41 +0100

    Added test for `bounadry` middleware

---
+++ src/middleware/boundary/__tests__/helpers/mock.js
@@ -0,0 +1,7 @@
+import { create, m } from '../../../../../src/index.js';
+
+create(
+    'x-example',
+    m.boundary(),
+    m.html(({  h}) => h('div', {} , "Hello Adam!"))
+);
--- src/middleware/boundary/__tests__/index.js
+++ src/middleware/boundary/__tests__/index.js
@@ -1,4 +1,5 @@
 import test from 'ava';
+import withComponent from 'ava-webcomponents';
 import { spy } from 'sinon';
 import defaultProps from '../../../../tests/helpers/default-props.js';
 import * as u from '../utils.js';
@@ -10,3 +11,29 @@ test('It should be able to invoke the `createBoundary` function;', t => {
     m(defaultProps);
     t.is(createBoundarySpy.callCount, 1);
 });
+
+test("It should be able to attach and reuse the shadow boundary;",
+withComponent(`${__dirname}/helpers/mock.js`),async (t, { page, utils })  => {
+    const name = 'x-example';
+
+    await utils.waitForUpgrade(name);
+
+    const hasBoundary = await page.evaluate(async name => {
+        const node = document.createElement(name);
+        document.body.append(node);
+        await node.idle();
+        window.lastShadowRoot = node.shadowRoot;
+        return Boolean(node.shadowRoot);
+    }, name);
+
+    t.true(hasBoundary);
+
+    const hasSameBoundary = await page.evaluate(async name => {
+        const node = document.querySelector(name);
+        await node.render();
+        return node.shadowRoot===window.lastShadowRoot;
+    }, name);
+
+    t.true(hasSameBoundary);
+    
+})
