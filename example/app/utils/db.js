994d83404b17ca922f3bcd796340955fae7b7576
Wildhoney <adam.timberlake@gmail.com>
2019-04-04 23:37:21 +0100

    Added try catch for 'indexedDB.open'

--- example/app/utils/db.js
+++ example/app/utils/db.js
@@ -4,56 +4,62 @@ const VERSION = 1;
 
 const MODE = { READWRITE: 'readwrite', READONLY: 'readonly' };
 
+const defaults = {
+    active: false,
+    add: () => {},
+    edit: () => {},
+    remove: () => {},
+    todos: []
+};
+
 export default () => {
     return new Promise(resolve => {
-        const open = window.indexedDB.open('database', VERSION);
+        try {
+            const open = window.indexedDB.open('database', VERSION);
 
-        open.addEventListener('upgradeneeded', () => {
-            if (!open.result.objectStoreNames.contains(NAME)) {
-                open.result.createObjectStore(NAME, { keyPath: 'id' });
-            }
-        });
-
-        open.addEventListener('error', () => {
-            // Continue without offline support.
-            resolve({
-                active: false,
-                add: () => {},
-                edit: () => {},
-                remove: () => {},
-                todos: []
+            open.addEventListener('upgradeneeded', () => {
+                if (!open.result.objectStoreNames.contains(NAME)) {
+                    open.result.createObjectStore(NAME, { keyPath: 'id' });
+                }
             });
-        });
 
-        open.addEventListener('success', () => {
-            const db = open.result;
+            open.addEventListener('error', () => {
+                // Continue without offline support.
+                resolve(defaults);
+            });
 
-            const add = model => {
-                const tx = db.transaction(NAME, MODE.READWRITE);
-                const store = tx.objectStore(NAME);
-                store.put(model);
-            };
+            open.addEventListener('success', () => {
+                const db = open.result;
 
-            const remove = model => {
-                const tx = db.transaction(NAME, MODE.READWRITE);
-                const store = tx.objectStore(NAME);
-                store.delete(model.id);
-            };
+                const add = model => {
+                    const tx = db.transaction(NAME, MODE.READWRITE);
+                    const store = tx.objectStore(NAME);
+                    store.put(model);
+                };
 
-            // Fetch all of the store todos in the database.
-            db.transaction(NAME, MODE.READONLY)
-                .objectStore(NAME)
-                .getAll()
-                .addEventListener('success', response => {
-                    // ...And then resolve the middleware, passing in the required properties.
-                    resolve({
-                        active: true,
-                        add,
-                        edit: add,
-                        remove,
-                        todos: response.target.result
+                const remove = model => {
+                    const tx = db.transaction(NAME, MODE.READWRITE);
+                    const store = tx.objectStore(NAME);
+                    store.delete(model.id);
+                };
+
+                // Fetch all of the store todos in the database.
+                db.transaction(NAME, MODE.READONLY)
+                    .objectStore(NAME)
+                    .getAll()
+                    .addEventListener('success', response => {
+                        // ...And then resolve the middleware, passing in the required properties.
+                        resolve({
+                            active: true,
+                            add,
+                            edit: add,
+                            remove,
+                            todos: response.target.result
+                        });
                     });
-                });
-        });
+            });
+        } catch {
+            resolve(defaults);
+        }
     });
 };
