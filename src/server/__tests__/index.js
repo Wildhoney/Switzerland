3e93e4de5ad885e6c2c0123e19c56eaf391cc726
Wildhoney <adam.timberlake@gmail.com>
2019-05-03 09:32:10 +0100

    Added support for passing attributes to the render fn

--- src/server/__tests__/index.js
+++ src/server/__tests__/index.js
@@ -1,9 +1,20 @@
 import test from 'ava';
 import { render } from '../index.js';
-import { create, m } from '../../index.js';
-
-const shallow = create('x-shallow', m.html(({ h }) => h('div', {}, 'Hello Adam!')));
+import { create, m, t as type } from '../../index.js';
 
 test('It should be able to render a shallow component to string;', async t => {
-    t.is(await render(shallow), '<x-shallow class="resolved"><div>Hello Adam!</div></x-shallow>');
+    const component = create('x-example', m.html(({ h }) => h('div', {}, 'Hello Adam!')));
+    t.is(await render(component), '<x-example class="resolved"><div>Hello Adam!</div></x-example>');
+});
+
+test('It should be able to render a shallow component with attributes to string;', async t => {
+    const component = create(
+        'x-example',
+        m.attrs({ name: type.String, age: type.Int }),
+        m.html(({ attrs, h }) => h('div', {}, `Hello ${attrs.name} you are ${attrs.age + 1} next!`))
+    );
+    t.is(
+        await render(component, { name: 'Maria', age: 28 }),
+        '<x-example name="Maria" age="28" class="resolved"><div>Hello Maria you are 29 next!</div></x-example>'
+    );
 });
--- src/server/index.js
+++ src/server/index.js
@@ -4,10 +4,16 @@ import { initialProps } from './utils.js';
 
 browserEnv(['window', 'document', 'navigator']);
 
-export async function render([name, middleware]) {
+export async function render([name, middleware], attrs = {}) {
     const node = document.createElement(name);
     const props = initialProps(node, middleware);
+
+    Object.entries(attrs).forEach(([name, value]) => {
+        node.setAttribute(name, value);
+    });
+
     await u.cycleMiddleware(node, props, middleware);
     node.classList.add('resolved');
+
     return node.outerHTML;
 }
