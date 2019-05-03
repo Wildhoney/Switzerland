f92b1ba82c343c5d8fc1df5d19b0b70ee8c5c101
Wildhoney <adam.timberlake@gmail.com>
2019-05-03 12:42:15 +0100

    Added @babel/plugin-syntax-import-meta

--- ava.config.js
+++ ava.config.js
@@ -1,4 +1,9 @@
 export default {
     failFast: true,
-    require: ['esm', 'node-url-imports', 'core-js', './tests/helpers/browser-env.js']
+    require: ['esm', 'node-url-imports', 'core-js', './tests/helpers/browser-env.js'],
+    babel: {
+        testOptions: {
+            plugins: ['@babel/plugin-syntax-import-meta']
+        }
+    }
 };
