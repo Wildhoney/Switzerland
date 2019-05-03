7830096bff1397aab43d45d2908366be3c6485ac
Wildhoney <adam.timberlake@gmail.com>
2019-05-03 10:25:18 +0100

    Exporting SSR render function

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import * as t from './types/index.js';
 import * as m from './middleware/index.js';
 
 export { create, alias, init } from './core/index.js';
+export { render } from './server/index.js';
 export { t, m };
