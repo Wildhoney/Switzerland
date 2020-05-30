e54a92d0751ae5035eca71ba8f3db75d90c68d8d
Wildhoney <adam.timberlake@gmail.com>
2020-05-30 17:25:03 +0100

    Changed window middleware

--- src/middleware/window/index.js
+++ src/middleware/window/index.js
@@ -1,22 +1,7 @@
-import { serverOptions } from '../../core/index.js';
 import { getWindow } from '../../utils.js';
-import * as utils from './utils.js';
 
-export default (url) => {
-    return async function window(props) {
-        const window = await getWindow();
-
-        if (!props.server) return { ...props, path: utils.getPath(url), window };
-
-        const path = await import('path');
-        const parsed = path.parse(url.replace('file://', ''));
-        const relative = path.relative(serverOptions.get('root'), parsed.dir);
-        const location = new URL(relative, serverOptions.get('path')).href;
-
-        return {
-            ...props,
-            path: utils.getPath(location.endsWith('/') ? location : `${location}/`),
-            window,
-        };
+export default function window() {
+    return async (props) => {
+        return { ...props, window: await getWindow() };
     };
-};
+}
