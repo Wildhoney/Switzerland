1acd9a448621ec3967ace4e6332472ce4f7b7c07
Wildhoney <adam.timberlake@gmail.com>
2020-05-30 15:10:04 +0100

    Fixed relative URLs in SSR mode

--- src/middleware/window/index.js
+++ src/middleware/window/index.js
@@ -4,17 +4,18 @@ import { getWindow } from '../../utils.js';
 
 export default (url) => {
     return async function window(props) {
-        const window = getWindow();
+        const window = await getWindow();
 
         if (!props.server) return { ...props, path: init(url), window };
 
         const path = await import('path');
         const parsed = path.parse(url.replace('file://', ''));
         const relative = path.relative(serverOptions.get('root'), parsed.dir);
+        const location = new URL(relative, serverOptions.get('path')).href;
 
         return {
             ...props,
-            path: init(new URL(relative, serverOptions.get('path'))),
+            path: init(location.endsWith('/') ? location : `${location}/`),
             window,
         };
     };
