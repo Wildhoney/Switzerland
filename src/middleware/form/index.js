40966ef7ea3416ed826be1f49e4aba6bb522150b
Wildhoney <adam.timberlake@gmail.com>
2020-06-02 09:40:09 +0100

    Improved the form middleware with await/async

--- src/middleware/form/index.js
+++ src/middleware/form/index.js
@@ -1,7 +1,7 @@
 import html from '../html/index.js';
 
 export default () => {
-    return function form(props) {
+    return async function form(props) {
         if (props.boundary) {
             // Gather all of the rendered forms so we can re-render on first mount.
             const forms = [...props.boundary.querySelectorAll('form')];
@@ -13,7 +13,7 @@ export default () => {
             // Only re-render when there are forms and there's a tree available.
             forms.length > 0 &&
                 props.html &&
-                html(props.html)({ ...props, form, lifecycle: 'update' });
+                (await html(props.html)({ ...props, form, lifecycle: 'update' }));
         }
 
         return props;
