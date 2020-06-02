e17030bad5f22e81751a3a8b45bef47d4f0ded07
Wildhoney <adam.timberlake@gmail.com>
2020-06-02 10:07:06 +0100

    Updated example to use the new middleware functions

--- example/app/nodes/todo-list/index.js
+++ example/app/nodes/todo-list/index.js
@@ -1,34 +1,52 @@
 import { create, m } from 'switzerland';
 import store from '../../utils/store.js';
 
-const middleware = [m.boundary(), store, m.path(import.meta.url), m.html(render)];
+const middleware = [
+    m.boundary(),
+    store,
+    m.window(),
+    m.history(),
+    m.path(import.meta.url),
+    m.html(render),
+];
 
-function render({ redux, h, path }) {
+function render({ h, redux, path, props }) {
+    const hasTodos = redux.state.list.length > 0;
+
+    return h('ul', {}, [
+        h.sheet(path('./styles/index.css')),
+        h.sheet(path('./styles/mobile.css'), '(max-width: 768px)'),
+        h.sheet(path('./styles/print.css'), 'print'),
+
+        ...(hasTodos ? List(props) : []),
+        ...(!hasTodos ? Nothing(props) : []),
+    ]);
+}
+
+function List({ history, redux, h }) {
     const todos = redux.state.list
         .filter((model) => (history.params.get('filter') ? !model.done : true))
         .sort((a, b) => a.created - b.created);
 
-    return h('ul', {}, [
-        todos.length === 0
-            ? h('li', { class: 'none' }, [h('p', {}, 'You are filtering out all completed todos.')])
-            : todos.map((model) =>
-                  h('li', { class: model.done ? 'done' : '' }, [
-                      h('p', { onclick: () => redux.actions.mark(model.id) }, model.text),
-                      h(
-                          'button',
-                          {
-                              class: 'delete',
-                              onclick: () => redux.actions.remove(model.id),
-                          },
-                          'Delete'
-                      ),
-                  ])
-              ),
+    return todos.length === 0
+        ? [h('li', { class: 'none' }, [h('p', {}, 'You are filtering out all completed todos.')])]
+        : todos.map((model) =>
+              h('li', { class: model.done ? 'done' : '' }, [
+                  h('p', { onClick: () => redux.actions.mark(model.id) }, model.text),
+                  h(
+                      'button',
+                      {
+                          class: 'delete',
+                          onClick: () => redux.actions.remove(model.id),
+                      },
+                      'Delete'
+                  ),
+              ])
+          );
+}
 
-        h.sheet(path('./styles/index.css')),
-        h.sheet(path('./styles/mobile.css'), '(max-width: 768px)'),
-        h.sheet(path('./styles/print.css'), 'print'),
-    ]);
+function Nothing({ h }) {
+    return [h('li', { class: 'none' }, [h('p', {}, 'You have not added any todos yet.')])];
 }
 
 export default create('todo-list', ...middleware);
