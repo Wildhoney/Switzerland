b6f47abe0b5187db425fc7db90cd2a7361a7a4a9
Wildhoney <adam.timberlake@gmail.com>
2020-07-30 19:54:13 +0100

    Added `subscribe-redux` adapter

---
+++ src/core/impl/adapters/subscribe-redux/index.js
@@ -0,0 +1,31 @@
+import * as r from 'redux';
+import t from 'redux-thunk';
+
+const redux = {
+    createStore: r.default?.createStore ?? r.createStore,
+    applyMiddleware: r.default?.applyMiddleware ?? r.applyMiddleware,
+    bindActionCreators: r.default?.bindActionCreators ?? r.bindActionCreators,
+};
+
+export const nodes = new WeakSet();
+
+export function newRedux(reducer, actions) {
+    const { dispatch, ...props } = redux.createStore(reducer, redux.applyMiddleware(t.default ?? t));
+    const boundActions = actions && redux.bindActionCreators(actions, dispatch);
+
+    return { ...props, dispatch, boundActions };
+}
+
+export default function subscribeRedux({ node, render }) {
+    return ({ getState, boundActions, dispatch, subscribe }) => {
+        const state = getState();
+        const redux = { state, actions: boundActions, dispatch };
+
+        if (!nodes.has(node)) {
+            subscribe(() => render({ redux }));
+            nodes.add(node);
+        }
+
+        return redux;
+    };
+}
