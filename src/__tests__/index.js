73ee7e65c054f5cc78d2652b654c2f3c4c547639
Wildhoney <adam.timberlake@gmail.com>
2020-08-09 18:40:51 +0100

    Formatted the HTML in the tests

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -65,7 +65,95 @@ test('it should be able to render a component tree;', async (t) => {
 
 test('It should be able to extract the stylesheets from a tree for preloading;', async (t) => {
     const html = `
-    <todo-app data-swiss=""><template shadowroot="open"><section class="todo-app"><todo-input data-swiss=""><template shadowroot="open"><form><input value="" type="text" name="todo" required="" minlength="3" autofocus="on" autocomplete="off" placeholder="What do you need to do?"><button type="submit" class="add" disabled=""></button></form><link key="http://localhost:3000/nodes/todo-input/styles/index.css" rel="stylesheet" type="text/css" href="http://localhost:3000/nodes/todo-input/styles/index.css"><link key="http://localhost:3000/nodes/todo-input/styles/mobile.css" rel="stylesheet" type="text/css" href="http://localhost:3000/nodes/todo-input/styles/mobile.css" media="(max-width: 768px)"><link key="http://localhost:3000/nodes/todo-input/styles/print.css" rel="stylesheet" type="text/css" href="http://localhost:3000/nodes/todo-input/styles/print.css" media="print"></template></todo-input><todo-list data-swiss=""><template shadowroot="open"><ul><li class="none"><p>Please wait for your todos to be fetched...</p></li></ul><link key="http://localhost:3000/nodes/todo-list/styles/index.css" rel="stylesheet" type="text/css" href="http://localhost:3000/nodes/todo-list/styles/index.css"><link key="http://localhost:3000/nodes/todo-list/styles/mobile.css" rel="stylesheet" type="text/css" href="http://localhost:3000/nodes/todo-list/styles/mobile.css" media="(max-width: 768px)"><link key="http://localhost:3000/nodes/todo-list/styles/print.css" rel="stylesheet" type="text/css" href="http://localhost:3000/nodes/todo-list/styles/print.css" media="print"></template></todo-list><h1 part="header"><a href="https://github.com/Wildhoney/Switzerland"><img src="http://localhost:3000/nodes/todo-app/images/logo.png"></a></h1><ul><li><em>Logo: </em><a class="">Bottom</a><span> / </span><a class="active">Top</a></li><li><em>Complete: </em><a class="active">Show</a><span> / </span><a class="">Hide</a></li><li>Loading...</li></ul></section><link key="http://localhost:3000/nodes/todo-app/styles/index.css" rel="stylesheet" type="text/css" href="http://localhost:3000/nodes/todo-app/styles/index.css"><link key="http://localhost:3000/nodes/todo-app/styles/mobile.css" rel="stylesheet" type="text/css" href="http://localhost:3000/nodes/todo-app/styles/mobile.css" media="(max-width: 768px)"><link key="http://localhost:3000/nodes/todo-app/styles/print.css" rel="stylesheet" type="text/css" href="http://localhost:3000/nodes/todo-app/styles/print.css" media="print"><style type="text/css">:host {  --order-position: -1; --border-colour: rgba(0, 0, 0, 0.1); }</style></template></todo-app>
+<todo-app data-swiss="">
+    <template shadowroot="open">
+        <section class="todo-app">
+            <todo-input data-swiss="">
+                <template shadowroot="open">
+                    <form>
+                        <input
+                            value=""
+                            type="text"
+                            name="todo"
+                            required=""
+                            minlength="3"
+                            autofocus="on"
+                            autocomplete="off"
+                            placeholder="What do you need to do?"
+                        /><button type="submit" class="add" disabled=""></button>
+                    </form>
+                    <link
+                        key="http://localhost:3000/nodes/todo-input/styles/index.css"
+                        rel="stylesheet"
+                        type="text/css"
+                        href="http://localhost:3000/nodes/todo-input/styles/index.css" /><link
+                        key="http://localhost:3000/nodes/todo-input/styles/mobile.css"
+                        rel="stylesheet"
+                        type="text/css"
+                        href="http://localhost:3000/nodes/todo-input/styles/mobile.css"
+                        media="(max-width: 768px)" /><link
+                        key="http://localhost:3000/nodes/todo-input/styles/print.css"
+                        rel="stylesheet"
+                        type="text/css"
+                        href="http://localhost:3000/nodes/todo-input/styles/print.css"
+                        media="print" /></template></todo-input
+            ><todo-list data-swiss=""
+                ><template shadowroot="open"
+                    ><ul>
+                        <li class="none"><p>Please wait for your todos to be fetched...</p></li>
+                    </ul>
+                    <link
+                        key="http://localhost:3000/nodes/todo-list/styles/index.css"
+                        rel="stylesheet"
+                        type="text/css"
+                        href="http://localhost:3000/nodes/todo-list/styles/index.css" /><link
+                        key="http://localhost:3000/nodes/todo-list/styles/mobile.css"
+                        rel="stylesheet"
+                        type="text/css"
+                        href="http://localhost:3000/nodes/todo-list/styles/mobile.css"
+                        media="(max-width: 768px)" /><link
+                        key="http://localhost:3000/nodes/todo-list/styles/print.css"
+                        rel="stylesheet"
+                        type="text/css"
+                        href="http://localhost:3000/nodes/todo-list/styles/print.css"
+                        media="print" /></template
+            ></todo-list>
+            <h1 part="header">
+                <a href="https://github.com/Wildhoney/Switzerland"
+                    ><img src="http://localhost:3000/nodes/todo-app/images/logo.png"
+                /></a>
+            </h1>
+            <ul>
+                <li><em>Logo: </em><a class="">Bottom</a><span> / </span><a class="active">Top</a></li>
+                <li><em>Complete: </em><a class="active">Show</a><span> / </span><a class="">Hide</a></li>
+                <li>Loading...</li>
+            </ul>
+        </section>
+        <link
+            key="http://localhost:3000/nodes/todo-app/styles/index.css"
+            rel="stylesheet"
+            type="text/css"
+            href="http://localhost:3000/nodes/todo-app/styles/index.css"
+        /><link
+            key="http://localhost:3000/nodes/todo-app/styles/mobile.css"
+            rel="stylesheet"
+            type="text/css"
+            href="http://localhost:3000/nodes/todo-app/styles/mobile.css"
+            media="(max-width: 768px)"
+        /><link
+            key="http://localhost:3000/nodes/todo-app/styles/print.css"
+            rel="stylesheet"
+            type="text/css"
+            href="http://localhost:3000/nodes/todo-app/styles/print.css"
+            media="print"
+        /><style type="text/css">
+            :host {
+                --order-position: -1;
+                --border-colour: rgba(0, 0, 0, 0.1);
+            }
+        </style></template
+    ></todo-app
+>
     `;
     t.snapshot(await preload(html));
 });
