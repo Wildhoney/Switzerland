a96e1f386664fa8001e02dcde8fed67b0ad93df3
Wildhoney <adam.timberlake@gmail.com>
2020-09-12 17:52:01 +0100

    Modified the `attachServiceWorker` adapter

--- src/adapters/attach-service-worker/index.js
+++ src/adapters/attach-service-worker/index.js
@@ -1,23 +1,26 @@
 /**
  * @function attachServiceWorker
+ * ---
+ * Attaches the service worker with the ability to pass a model to the worker via the second argument.
+ * If the second argument is a function then it's invoked, which allows you to parse the HTML from
+ * the node after it has been rendered.
  */
 export default function attachServiceWorker({ boundableAdapters, ...props }) {
-    return (path) => {
+    return (path, getModel = {}) => {
         if (typeof window === 'undefined') return null;
 
-        const { node } = props;
         const run = boundableAdapters.run(props);
 
         run.onRender(() => {
-            const resources = [...(node.shadowRoot ?? node).querySelectorAll('link')].map((link) =>
-                link.getAttribute('href')
+            const params = new URLSearchParams();
+            const model = typeof getModel === 'function' ? getModel(props.node) : getModel;
+
+            Object.entries(model).forEach(([key, value]) =>
+                params.set(key, typeof value === 'object' ? JSON.stringify(value) : value)
             );
 
-            const params = new URLSearchParams();
-            params.set('origin', window.location.origin);
-            params.set('resources', JSON.stringify(resources.map((resource) => new URL(resource).pathname)));
-
-            const url = new URL(`?${params.toString()}`, path).href;
+            const args = Object.values(model).length === 0 ? '' : `?${params.toString()}`;
+            const url = new URL(args, path).href;
             navigator.serviceWorker.register(url);
         });
     };
