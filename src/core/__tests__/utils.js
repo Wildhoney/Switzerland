9d06faeb7df4624d3d3d7732290be738d1c16b62
Adam Timberlake <adam.timberlake@clearscore.com>
2023-03-11 18:30:15 +0000

    Removed version 'Geneva'

--- src/core/__tests__/utils.js
+++
@@ -1,8 +0,0 @@
-import test from 'ava';
-import { getDefaultView } from '../utils.js';
-
-test('It should be able to initialise the default view;', (t) => {
-    const node = document.createElement('x-view');
-    const props = getDefaultView({ node });
-    t.snapshot(props);
-});
