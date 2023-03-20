f66fab85bab40f6e7bf722099c12d87ca249dd3f
Adam Timberlake <adam.timberlake@clearscore.com>
2023-03-20 23:15:12 +0000

    chore: added code coverage without threshold (for now)

--- packages/library/jest.config.ts
+++ packages/library/jest.config.ts
@@ -3,12 +3,20 @@ import type { Config } from "@jest/types";
 const config: Config.InitialOptions = {
   testEnvironment: "jsdom",
   transform: {
-    "\\.[jt]sx?$": ["ts-jest", { useESM: true }],
+    "\\.tsx?$": ["ts-jest", { useESM: true }],
   },
-  moduleNameMapper: {
-    "(.+)\\.js": "$1",
+  moduleNameMapper: { "(.+)\\.js": "$1" },
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  collectCoverageFrom: ["./src/**/*.{ts,tsx}"],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
   },
-  extensionsToTreatAsEsm: [".ts"],
 };
 
 export default config;
